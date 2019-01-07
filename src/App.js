9cbac1c92733c1fde67ab0a3991b4d8662c2dce0
Gaston Graciani <gastongraciani@gmail.com>
2019-01-07 19:04:51 -0300

    Create database connection and google authentification

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      user: null
+    }
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.renderLoginButton = this.renderLoginButton.bind(this);
+    this.handleLogout = this.handleLogout.bind(this)
+  }
+
+  componentWillMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        user
+      });
+    });
+  }
+
+  handleAuth(){
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(res => console.log(`${res.user.email} ha iniciado sesion`))
+      .catch(err => console.error(err));
+  }
+
+  handleLogout(){
+    firebase.auth().signOut()
+    .then(res => console.log(`${res.user.email} ha salido`))
+    .catch(err => console.error(err));
+  }
+
+  renderLoginButton(){
+    // Si el user esta logeado
+    if(this.state.user){
+      return (
+        <div>
+          <img src={this.state.user.photoURL} alt="{this.state.user.displayName}"/>
+          <p>Hola {this.state.user.displayName}</p>
+          <button onClick={this.handleLogout} >Sign out</button>
+        </div>
+      );
+    } else {
+      // Y si no lo esta hace otra cosa
+      return(
+        <button onClick={this.handleAuth}>Sign in</button>
+      )
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <h2>Freegram</h2>
+        <div className="div">
+          <p className="App-intro">
+            {this.renderLoginButton()}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import firebase from 'firebase';
+
+firebase.initializeApp({
+    apiKey: "AIzaSyABsPapd8O9TfbMtdRwczCtsvcN1K3SeuI",
+    authDomain: "freegram-d6640.firebaseapp.com",
+    databaseURL: "https://freegram-d6640.firebaseio.com",
+    projectId: "freegram-d6640",
+    storageBucket: "freegram-d6640.appspot.com",
+    messagingSenderId: "287037150898"
+})
+
+ReactDOM.render(
+    <App />, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
